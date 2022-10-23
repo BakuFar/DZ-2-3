a60453550550cfe1d2d576b6d24eb28703354a3b
Farhat <baku.far@gmail.com>
2022-10-23 18:39:10 +0600

    Initial Commit

--- DZ#2-3/main.swift
+++ DZ#2-3/main.swift
@@ -17,39 +17,6 @@ import Foundation
 //Пример:
 //#1 - Имя Фамилия - Возраст - Класс - Средний балл
 
-class Student{
-    var name: String = ""
-    var surname: String = ""
-    var age: Int = 0
-    var grade: Int = 0
-    var avarageScore: Int = 0
-    
-    init(name: String, surname: String, age: Int, grade: Int, avarageScore: Int) {
-        self.name = name
-        self.surname = surname
-        self.age = age
-        self.grade = grade
-        self.avarageScore = avarageScore
-    }
-}
-var edil = Student(name: "Edil", surname: "Baysalov", age: 16, grade: 9, avarageScore: 78)
-var vlad = Student(name: "Vladislav", surname: "Voskoboynikov", age: 14, grade: 7, avarageScore: 86)
-var meder = Student(name: "Meder", surname: "Bazarbaev", age: 13, grade: 7, avarageScore: 77)
-
-class Teacher{
-    var name: String = ""
-    var surname: String = ""
-    var age: Int = 0
-    
-    init(name: String, surname: String, age: Int) {
-        self.name = name
-        self.surname = surname
-        self.age = age
-    }
-}
-let dayir = Teacher(name: "Dayir", surname: "Duyshobaev", age: 32)
-let asyl = Teacher(name: "Asylbek", surname: "Agay", age: 42)
-
 class Database {
     private var studentsArray: [Student] = []
     private var studentsString: String = ""
@@ -58,7 +25,7 @@ class Database {
     private var teachersString: String = ""
     private var teachersCount: Int = 0
 
-    func addToStudentsArray(student: Student) {
+    func addStudent(student: Student) {
         studentsArray.append(student)
         studentsCount += 1
         studentsString += "\nStudent #\(studentsCount)\nname - \(student.name)\nsurname - \(student.surname)\nage - \(student.age)\ngrade - \(student.grade)\navarage score - \(student.avarageScore)\n"
@@ -66,7 +33,7 @@ class Database {
     func showStudentsInfo() {
         print(studentsString)
     }
-    func addToTeachersArray(teacher: Teacher) {
+    func addTeacher(teacher: Teacher) {
         teachersArray.append(teacher)
         teachersCount += 1
         teachersString += "\nTeacher #\(teachersCount)\nname - \(teacher.name)\nsurname - \(teacher.surname)\nage - \(teacher.age)\n"
@@ -76,10 +43,16 @@ class Database {
     }
 }
 let database = Database()
-database.addToStudentsArray(student: vlad)
-database.addToStudentsArray(student: edil)
-database.addToStudentsArray(student: meder)
+let edil = Student(name: "Edil", surname: "Baysalov", age: 16, grade: 9, avarageScore: 78)
+let vlad = Student(name: "Vladislav", surname: "Voskoboynikov", age: 14, grade: 7, avarageScore: 86)
+let meder = Student(name: "Meder", surname: "Bazarbaev", age: 13, grade: 7, avarageScore: 77)
+let dayir = Teacher(name: "Dayir", surname: "Duyshobaev", age: 32)
+let asyl = Teacher(name: "Asylbek", surname: "Agay", age: 42)
+
+database.addStudent(student: vlad)
+database.addStudent(student: edil)
+database.addStudent(student: meder)
 database.showStudentsInfo()
-database.addToTeachersArray(teacher: dayir)
-database.addToTeachersArray(teacher: asyl)
+database.addTeacher(teacher: dayir)
+database.addTeacher(teacher: asyl)
 database.showTeachersInfo()
